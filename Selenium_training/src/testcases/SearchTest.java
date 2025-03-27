8d20864bde04f9a7011a480453901852d4c87c10
Aditya goswami <ag2881427@gmail.com>
2025-03-27 15:18:09 +0530

    conflict

--- Selenium_training/src/testcases/SearchTest.java
+++ Selenium_training/src/testcases/SearchTest.java
@@ -23,20 +23,4 @@ public class SearchTest {
         System.out.println("executed");
         System.out.println("2nd exectuion");
     }
-	@Test
-	public void searchtestcase()
-	{
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://demowebshop.tricentis.com/");
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		Reporter.log("app launched successfully");
-		driver.findElement(By.id("small-searchterms")).sendKeys("computers");
-		driver.findElement(By.xpath("//input[@type='submit']")).click();
-		Reporter.log("search test case completed");
-		driver.quit();
-		
-		
-	}
-
 }
